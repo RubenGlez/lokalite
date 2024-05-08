38a928805f1545eafc22d82aae67ffb4ff254fd3
RubenGlez <ruben.daren@gmail.com>
2024-05-08 12:05:19 +0200

    feat: shacn setup

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ export async function GET(request: Request) {
   return Response.json({
     data: {
       message: "It's works!",
-      from: request.url,
+      url: request.url,
     },
   });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="p-24">
-      <h1 className="text-xl mb-2">Home page</h1>
+    <main className="flex flex-col p-24 gap-4">
+      <h1 className="text-xl">Home page</h1>
       <p className="text-sm text-slate-500">
         This page is public, an is the main entry point for all users
       </p>
+
+      <div className="flex gap-4">
+        <Button>SignIn</Button>
+        <Button variant="outline">SignUp</Button>
+      </div>
     </main>
   );
 }
