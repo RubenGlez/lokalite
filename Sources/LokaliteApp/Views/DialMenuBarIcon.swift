cb692879535db34e37b976ea39d05666c3b986b3
RubenGlez <ruben.daren@gmail.com>
2026-05-22 14:09:02 +0200

    Fix release build for menu bar icon

--- Sources/LokaliteApp/Views/DialMenuBarIcon.swift
+++ Sources/LokaliteApp/Views/DialMenuBarIcon.swift
@@ -13,47 +13,69 @@ struct DialMenuBarIcon: View {
 
     static func templateImage(size: CGFloat = 18) -> NSImage {
         let image = NSImage(size: NSSize(width: size, height: size), flipped: false) { rect in
-            let cx = rect.width / 2
-            let cy = rect.height / 2
-            let outerR = min(cx, cy) - 1.25
-
-            NSColor.black.setStroke()
-            NSColor.black.setFill()
-
-            // Outer bezel ring
-            let outerPath = NSBezierPath(ovalIn: CGRect(
-                x: cx - outerR, y: cy - outerR, width: outerR * 2, height: outerR * 2))
-            outerPath.lineWidth = 2.0
-            outerPath.stroke()
-
-            // 12 tick marks
-            for i in 0..<12 {
-                let angle = Double(i) * (.pi * 2 / 12) - .pi / 2
-                let major = i % 3 == 0
-                let r1 = outerR - 1.0
-                let r2 = r1 - (major ? 4.0 : 2.5)
-                let tick = NSBezierPath()
-                tick.move(to: CGPoint(x: cx + r1 * cos(angle), y: cy + r1 * sin(angle)))
-                tick.line(to: CGPoint(x: cx + r2 * cos(angle), y: cy + r2 * sin(angle)))
-                tick.lineWidth = major ? 1.5 : 1.1
-                tick.stroke()
-            }
-
-            // Inner dial ring
-            let dialR = outerR * 0.46
-            let dialPath = NSBezierPath(ovalIn: CGRect(
-                x: cx - dialR, y: cy - dialR, width: dialR * 2, height: dialR * 2))
-            dialPath.lineWidth = 1.5
-            dialPath.stroke()
-
-            // Centre dot
-            let dotR: CGFloat = 2.25
-            NSBezierPath(ovalIn: CGRect(
-                x: cx - dotR, y: cy - dotR, width: dotR * 2, height: dotR * 2)).fill()
-
+            drawTemplateIcon(in: rect)
             return true
         }
         image.isTemplate = true
         return image
+    }
+
+    private static func drawTemplateIcon(in rect: CGRect) {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let outerRadius = min(rect.width, rect.height) / 2 - 1.25
+
+        NSColor.black.setStroke()
+        NSColor.black.setFill()
+
+        drawRing(center: center, radius: outerRadius, lineWidth: 2.0)
+        drawTicks(center: center, radius: outerRadius)
+        drawRing(center: center, radius: outerRadius * 0.46, lineWidth: 1.5)
+        drawDot(center: center, radius: 2.25)
+    }
+
+    private static func drawRing(center: CGPoint, radius: CGFloat, lineWidth: CGFloat) {
+        let diameter = radius * 2
+        let rect = CGRect(
+            x: center.x - radius,
+            y: center.y - radius,
+            width: diameter,
+            height: diameter
+        )
+        let path = NSBezierPath(ovalIn: rect)
+        path.lineWidth = lineWidth
+        path.stroke()
+    }
+
+    private static func drawTicks(center: CGPoint, radius: CGFloat) {
+        for index in 0..<12 {
+            let angle = CGFloat(index) * (.pi * 2 / 12) - .pi / 2
+            let isMajorTick = index % 3 == 0
+            let outerTickRadius = radius - 1.0
+            let innerTickRadius = outerTickRadius - (isMajorTick ? 4.0 : 2.5)
+            let tick = NSBezierPath()
+
+            tick.move(to: point(on: center, radius: outerTickRadius, angle: angle))
+            tick.line(to: point(on: center, radius: innerTickRadius, angle: angle))
+            tick.lineWidth = isMajorTick ? 1.5 : 1.1
+            tick.stroke()
+        }
+    }
+
+    private static func drawDot(center: CGPoint, radius: CGFloat) {
+        let diameter = radius * 2
+        let rect = CGRect(
+            x: center.x - radius,
+            y: center.y - radius,
+            width: diameter,
+            height: diameter
+        )
+        NSBezierPath(ovalIn: rect).fill()
+    }
+
+    private static func point(on center: CGPoint, radius: CGFloat, angle: CGFloat) -> CGPoint {
+        CGPoint(
+            x: center.x + radius * cos(angle),
+            y: center.y + radius * sin(angle)
+        )
     }
 }
