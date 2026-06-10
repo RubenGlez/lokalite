f120441e5e058236d66c99bd620ef5ff647295de
RubenGlez <ruben.daren@gmail.com>
2026-06-10 14:49:26 +0200

    feat: armadillo head menu bar icon
    Replace the placeholder rounded-square glyph with the armadillo head
    mark from the brand artwork, rendered as a proper template image so
    macOS tints it for light/dark menu bars and the highlighted state.

--- Sources/LokaliteApp/Views/MenuBarIcon.swift
+++ Sources/LokaliteApp/Views/MenuBarIcon.swift
@@ -12,40 +12,21 @@ struct MenuBarIcon: View {
     }
 
     static func templateImage(size: CGFloat = 18) -> NSImage {
+        let artwork = Self.artwork
         let image = NSImage(size: NSSize(width: size, height: size), flipped: false) { rect in
-            NSGraphicsContext.current?.shouldAntialias = true
-            drawIcon(in: rect)
+            NSGraphicsContext.current?.imageInterpolation = .high
+            artwork.draw(in: rect, from: .zero, operation: .sourceOver, fraction: 1)
             return true
         }
         image.isTemplate = true
         return image
     }
 
-    private static func drawIcon(in rect: CGRect) {
-        let s = rect.width / 18
-
-        NSColor.black.setFill()
-        NSColor.black.setStroke()
-
-        // Rounded square outline
-        let squareRect = CGRect(x: rect.minX + s, y: rect.minY + s, width: 16 * s, height: 16 * s)
-        let roundedPath = NSBezierPath(roundedRect: squareRect, xRadius: 3 * s, yRadius: 3 * s)
-        roundedPath.lineWidth = 1.5 * s
-        roundedPath.stroke()
-
-        // Converts SVG coordinates (y-down, origin top-left) to AppKit (y-up)
-        func pt(_ x: CGFloat, _ y: CGFloat) -> NSPoint {
-            NSPoint(x: rect.minX + x * s, y: rect.minY + (18 - y) * s)
+    private static let artwork: NSImage = {
+        guard let url = Bundle.module.url(forResource: "MenuBarIcon", withExtension: "png"),
+              let image = NSImage(contentsOf: url) else {
+            return NSImage(size: NSSize(width: 18, height: 18))
         }
-
-        // Rounded "L" drawn as a stroked path
-        let lPath = NSBezierPath()
-        lPath.move(to: pt(6, 4.5))
-        lPath.line(to: pt(6, 13.5))
-        lPath.line(to: pt(12, 13.5))
-        lPath.lineWidth = 2 * s
-        lPath.lineCapStyle = .round
-        lPath.lineJoinStyle = .round
-        lPath.stroke()
-    }
+        return image
+    }()
 }
